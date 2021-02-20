daf770a3061683f78ea818fa7ca7be43223095f6
William Jensen <williambjensen01@gmail.com>
2021-02-19 17:28:25 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,25 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement > 0 && item.enhancement < 20) {
+    item.enhancement ++;
+  }
+  return { ...item }
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5
+  } else if (item.enhacement >= 15) {
+    item.durability -= 10
+  } else if (item.enhancement > 16) {
+    item.durability -= 1
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
